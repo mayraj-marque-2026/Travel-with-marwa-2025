df0cf412bf8843fb4756d55c8f7b55422d45fdc0
mayraj-marque-2026 <asadlastname@gmail.com>
2026-07-29 02:27:45 +0500

    Update tourData.ts

--- src/data/tourData.ts
+++ src/data/tourData.ts
@@ -8,7 +8,7 @@ import {
   FAQItem,
 } from '../types';
 import naranImage from '../assets/images/naran_mountain_road_1785142096421.jpg';
-import raftingImage from '../assets/images/kunhar_river_rafting_1785142398624.jpg';
+import raftingImage from '../assets/images/kunhar_river.jpg';
 import busDepartureImage from '../assets/images/luxury_bus_departure_marwa_1785142839368.jpg';
 
 export const BUSINESS_INFO = {
